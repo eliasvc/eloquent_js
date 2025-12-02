6d436cf992d7e465c0e498c5c385057aeb4f382e
Elias  Valdez <elias.vc@gmail.com>
2025-12-01 19:22:15 -0500

    Added chapter3 minimum solution

---
+++ chapter3/minimum.js
@@ -0,0 +1,7 @@
+// Define the function min that takes two arguments and returns their minimum.
+function minimum(a, b) {
+  return a < b ? a : b;
+}
+
+console.log(`The minimum number between 3 and 5 is ${minimum(3, 5)}`);
+console.log(`The minimum number between 6 and 2 is ${minimum(6, 2)}`);
