b5c148bbb18e67c2e9e423f85065e4f17858220c
Elias Valdez <evaldez@linode.com>
2025-12-18 16:52:27 -0500

    Solution to chapter 4, list exercise

--- chapter4/list.js
+++ chapter4/list.js
@@ -17,4 +17,34 @@ function arrayToList(array) {
   return list;
 }
 
+function listToArray(list) {
+  let array = [];
+  let pointer = list;
+  while (pointer != null) {
+    array.push(pointer.value);
+    pointer = pointer.rest;
+  }
+
+  return array;
+}
+
+function prepend(element, list) {
+  return { value: element, rest: list };
+}
+
+function nth(list, n) {
+  let nthList = list;
+  for (let i = 0; i < n; i++) {
+    nthList = nthList?.rest;
+  }
+
+  return nthList?.value;
+}
+
 console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 10));
+console.log(nth(arrayToList([10, 20, 30]), 0));
+console.log(nth(arrayToList([10, 20, 30]), 2));
+console.log(nth(arrayToList([]), 2));
